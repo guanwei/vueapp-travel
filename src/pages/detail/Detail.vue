2e6602781f4c832119ad9019b3287f1c6a8cb61b
Edward Guan <285006386@qq.com>
2020-03-30 21:56:51 +0800

    banner finish

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Detail">
+    <detail-banner></detail-banner>
+  </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner'
+export default {
+  name: 'Detail',
+  components: {
+    DetailBanner
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
